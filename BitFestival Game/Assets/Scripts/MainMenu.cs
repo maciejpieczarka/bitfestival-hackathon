2d0713f6957a8fcbc9ad68c3b6d19bf414991a5c
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2024-11-30 21:40:13 +0100

    Complete main menu and menu music

--- BitFestival Game/Assets/Scripts/MainMenu.cs
+++ BitFestival Game/Assets/Scripts/MainMenu.cs
@@ -2,15 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    private void Start()
+    {
+        LoadVolume();
+        MusicManager.Instance.PlayMusic("MainMenu");
+    }
+
     public void Play() 
     {
         SceneManager.LoadScene(0);
+        MusicManager.Instance.PlayMusic("MainMenu");
     }
     public void Quit() 
     {
         Application.Quit();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveValue()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
